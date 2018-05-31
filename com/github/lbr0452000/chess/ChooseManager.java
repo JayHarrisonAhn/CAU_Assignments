c10c0b65226cfcee4cee0f80b6a1011125bbac70
lbr0452000 <lbr0452000@naver.com>
2018-05-31 16:05:08 +0900

    Game1Manager commit added

--- com/github/lbr0452000/chess/ChooseManager.java
+++ com/github/lbr0452000/chess/ChooseManager.java
@@ -11,42 +11,40 @@ import javax.swing.JPanel;
 import com.github.lbr0452000.chess.panels.ChessBoard;
 import com.github.lbr0452000.chess.panels.ChessBoard1;
 
-public class ChooseManager extends JFrame {
+public class ChooseManager extends JFrame {//게임 모드 선택하는 매니
 	ChessBoard board;
 	
 	public ChooseManager() {
 		super();
 		
-	    setSize(400, 300);
-	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    setLocationRelativeTo(null);
+	    setSize(400, 300);//화면 크기를 지
+	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//x버튼 누르면 프로그램 종료되도록 설
+	    setLocationRelativeTo(null);//???
 	    
-	    JButton mode1 = new JButton("1 vs 1");
-		JButton mode2 = new JButton("2 vs 2");
+	    JButton mode1 = new JButton("1 vs 1");//1대1버튼 생성 
+		JButton mode2 = new JButton("2 vs 2");//2대2버튼 생성 
 	    
-		mode1.addActionListener(new startMode1());
+		mode1.addActionListener(new startMode1());//1대1버튼이 눌러지면 startMode1() 실행 
 		mode2.addActionListener(new startMode2());
 		
-		add(mode1);
+		add(mode1);//1대1버튼을 화면에 추가 
 		add(mode2);
 		
-		setLayout(new FlowLayout(FlowLayout.CENTER, 0, 150));
-		setVisible(true);
+		setLayout(new FlowLayout(FlowLayout.CENTER, 0, 150));//레이아웃 설정 
+		setVisible(true);//화면이 보이도록 설정 
 
 	}
 	
 	private class startMode1 implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
-			System.out.println("1대1모드 실행");
-			dispose();
-			new Game1Manager();
+			System.out.println("1대1모드 실행");//콘솔창에 출력 
+			dispose();//현재창은 종료(닫기) 
+			new Game1Manager();//게임1매니저 생
 		}
 	}
 	
 	private class startMode2 implements ActionListener{
 		public void actionPerformed(ActionEvent e) {
-			// TODO Auto-generated method stub
 			System.out.println("2대2모드 실행");
 		}
 	}
--- com/github/lbr0452000/chess/Game1Manager.java
+++ com/github/lbr0452000/chess/Game1Manager.java
@@ -13,58 +13,54 @@ import com.github.lbr0452000.chess.panels.ChessBoard1;
 import com.github.lbr0452000.chess.panels.ChessBoardCell;
 import com.github.lbr0452000.chess.panels.Position;
 
-public class Game1Manager extends JFrame implements ActionListener{
+public class Game1Manager extends JFrame implements ActionListener{//Game1Manager는 JFrame을 상속받는 동시에 리스너의 역할도 수행합니다.
 
-	private ChessBoard1 board = new ChessBoard1();
-	
-	ChessPiece onHand;
+	private ChessBoard1 board = new ChessBoard1();//내부 프로퍼티인 보드 생성 
+	ChessPiece onHand;//onHand 생성 
 	
 	public Game1Manager() {
 		setSize(800, 800);
 	    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    setLocationRelativeTo(null);
 		
-		add(board);
+		add(board);//보드 추가 
 		setLayout(new GridLayout(1, 1));
 		
-		for(int i=0;i<8;i++) {
+		for(int i=0;i<8;i++) {//각각의 셀의 리스너로 자기 자신을 추가(각각의 셀이 눌리면 밑에 있는 actionPerformed가 실행된다.) 
 			for(int j=0;j<8;j++) {
 				board.cells[i][j].addActionListener(this);
 			}
 		}
-		
-		
-		
-		
 		
 		setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		ChessBoardCell selected = (ChessBoardCell) e.getSource();
-		
-		
-		if(onHand != null) {//손에 들고있을 상
-			selected.piece = onHand;
-			onHand = null;
-			board.refresh();
+		ChessBoardCell selected = (ChessBoardCell) e.getSource();//selected라는 레퍼런스 선언(현재 버튼이 눌린 ChessBoardCell을 갖는다)  
+		if(selected.piece == null) {		//TODO : 아무것도 없는 칸을 선택했을 경우
+			
 		}
-		else {
-			onHand = selected.piece;
-			System.out.print(selected.position.x);
+		
+		if(onHand != null) {			//손(onHand)에 무언가 들고있을 상황 
+			selected.piece = onHand;	// onHand에 있는 ChessPiece를 selected.piece에 집어넣는다.
+			onHand = null;				//onHand를 비운다. 
+			board.refresh();			//board를 새로 띄운다(새로고침)
+		}
+		else {//손(onHand)에 아무것도 들고있지 않은 상황 
+			onHand = selected.piece;//선택된 칸의 piece를 손(onHand)에 넣는다. 
+			System.out.print(selected.position.x);//콘솔에 선택된 위치를 출력시킨다. 
 			System.out.print(",");
 			System.out.println(selected.position.y);
 			
-			selected.piece = null;
-			selected.setBackground(Color.YELLOW);
+			selected.piece = null;//선택된 ChessBoardCell의 piece를 비운다.
+			selected.setBackground(Color.YELLOW);//선택된 ChessBoardCell을 노란색으로 채운다.
 		}
 		
 		
 	}
 	
-	void move(Position from, Position to) {
+	void move(Position from, Position to) {//TODO
 		
 	}
 }
