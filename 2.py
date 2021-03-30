0614c2b7b04dee584cdfd0d815edfc146f75116f
lbr0452000 <lbr0452000@naver.com>
2021-03-30 16:42:09 +0000

    Assignment #2 Clear

--- 2.py
+++ 2.py
@@ -5,3 +5,15 @@
 # Display the results when you put two different words (one is palindrome and
 # the other is not).
 
+word = input()
+isPalindrome = True
+
+for i in range(0, int(len(word)/2)):
+    if word[i] != word[len(word)-1-i]:
+        isPalindrome = False
+        break
+    
+if isPalindrome:
+    print(f"{word} is Palindrome")
+else:
+    print(f"{word} is NOT Palindrome")
