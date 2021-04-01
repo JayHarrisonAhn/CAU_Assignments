a2d9b0f3fdc115c02dbe65232c482072085412df
lbr0452000 <lbr0452000@naver.com>
2021-04-01 08:53:55 +0000

    Assignment #5 Clear

--- 5.py
+++ 5.py
@@ -2,3 +2,21 @@
 # to rotate the image by 90 degrees (clockwise). You can generate a matrix
 # randomly.
 
+import random
+
+
+originalMatrix = [[0 for col in range(5)] for row in range(5)]
+print("Original Matrix : ")
+for i in range(0, len(originalMatrix)):
+    for j in range(0, len(originalMatrix[i])):
+        originalMatrix[i][j] = random.randrange(0, 256)
+    print(originalMatrix[i])
+
+
+print("\nRotated Matrix : ")
+rotatedMatrix = [[0 for col in range(5)] for row in range(5)]
+for i in range(0, len(rotatedMatrix)):
+    for j in range(0, len(rotatedMatrix[i])):
+        rotatedMatrix[i][j] = originalMatrix[len(originalMatrix)-1-j][i]
+    print(rotatedMatrix[i])
+
