fb447fb0146895d6bfb4f0979936df7cf3bda24f
lbr0452000 <lbr0452000@naver.com>
2021-04-02 13:39:34 +0000

    Assignment #6 Clear

--- 6.py
+++ 6.py
@@ -4,3 +4,15 @@
 # Function: pairSum([2, 4, 3, 5, 6, -2, 4, 7, 8, 9], 7)
 # Output: [‘2+5’, ‘4+3’, ‘3+4’, ‘-2+9’]
 
+
+def pairSum(arr, num):
+    result = []
+    for i in range(0, len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i]+arr[j] == num:
+                result.append(f'{arr[i]}+{arr[j]}')
+    print(result)
+    return result
+
+
+pairSum([2, 4, 3, 5, 6, -2, 4, 7, 8, 9], 7)
