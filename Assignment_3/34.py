ca4e55569f2643a886f926ef23cc27d0220e3799
lbr0452000 <lbr0452000@naver.com>
2021-05-05 13:54:02 +0900

    Assignment#3 34.py code 정리

--- Assignment_3/34.py
+++ Assignment_3/34.py
@@ -1,12 +1,15 @@
-def find_descendant_indices(tree, node_index):
+def descendant_indices(tree, node_index):
     if node_index >= len(tree):
         return []
     elif tree[node_index] is None:
         return []
     else:
-        return [node_index]+find_descendant_indices(tree, (node_index*2)+1)+find_descendant_indices(tree, (node_index*2)+2)
+        return [node_index] \
+               + descendant_indices(tree, (node_index * 2) + 1) \
+               + descendant_indices(tree, (node_index * 2) + 2)
 
-def find_ancestor_indices(tree, node_index):
+
+def ancestor_indices(_, node_index):
     ancestors = [node_index]
     latest_ancestor_index = node_index
     while latest_ancestor_index > 0:
@@ -18,6 +21,7 @@ def find_ancestor_indices(tree, node_index):
     print(ancestors)
     return ancestors
 
+
 def check_if_CBT(tree):
     i = 0
     while i < len(tree):
@@ -26,29 +30,26 @@ def check_if_CBT(tree):
             i += 1
             continue
         else:
-            left_node_values = list(map(lambda index: tree[index], find_descendant_indices(tree, (i * 2) + 1)))
+            left_node_values = list(map(lambda index: tree[index], descendant_indices(tree, (i * 2) + 1)))
             if len(list(filter(lambda value: tree[i] < value, left_node_values))) > 0:
-                print("1This is NOT a valid Binary Search Tree")
+                print("This is NOT a valid Binary Search Tree")
                 return
 
-            right_node_values = list(map(lambda index: tree[index], find_descendant_indices(tree, (i * 2) + 2)))
+            right_node_values = list(map(lambda index: tree[index], descendant_indices(tree, (i * 2) + 2)))
             if len(list(filter(lambda value: tree[i] > value, right_node_values))) > 0:
-                print("2This is NOT a valid Binary Search Tree", right_node_values)
+                print("This is NOT a valid Binary Search Tree", right_node_values)
                 return
             i += 1
     print("This is a valid Binary Search Tree")
 
+
 def find_lowest_common_ancestor(tree, node1_index, node2_index):
-    node1_ancestors = find_ancestor_indices(tree, node1_index)
-    node2_ancestors = find_ancestor_indices(tree, node2_index)
+    node1_ancestors = ancestor_indices(tree, node1_index)
+    node2_ancestors = ancestor_indices(tree, node2_index)
     i = 0
     while i < len(node1_ancestors):
         if node1_ancestors[i] != node2_ancestors[i]:
-            return node1_ancestors[i-1]
+            return node1_ancestors[i - 1]
         else:
             i += 1
-    return node1_ancestors[i-1]
-
-
-inputTree = [8, 3, 9, None, None, 4, 7]
-check_if_CBT(inputTree)
+    return node1_ancestors[i - 1]
