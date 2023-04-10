c9226a8b1ed9ddefac7ec8d6b9c3607b3bde3ad5
Jay Ahn <jay@xerapix.studio>
2023-04-10 13:53:15 +0900

    pc_dynamic

--- proj1/problem1/pc_dynamic.java
+++ proj1/problem1/pc_dynamic.java
@@ -1,6 +1,9 @@
 package problem1;
 
-public class pc_static_cyclic {
+import javax.swing.text.html.Option;
+import java.util.OptionalInt;
+
+public class pc_dynamic {
     private static final int NUM_START = 0;
     private static int NUM_END = 200000;
     private static int NUM_THREADS = 4;
@@ -11,14 +14,15 @@ public class pc_static_cyclic {
             NUM_THREADS = Integer.parseInt(args[0]);
             NUM_END = Integer.parseInt(args[1]);
         }
-        pc_static_cyclic_counter counter = new pc_static_cyclic_counter();
+        pc_dynamic_counter counter = new pc_dynamic_counter();
         int i;
         long startTime = System.currentTimeMillis();
 
         // Thread creation
-        pc_static_cyclic_thread[] threads = new pc_static_cyclic_thread[NUM_THREADS];
+        pc_dynamic_task_stack task_stack = new pc_dynamic_task_stack(NUM_START, NUM_END, SIZE_OF_TASK);
+        pc_dynamic_thread[] threads = new pc_dynamic_thread[NUM_THREADS];
         for (i=0; i<NUM_THREADS; i++) {
-            threads[i] = new pc_static_cyclic_thread(i, NUM_THREADS, SIZE_OF_TASK, NUM_START, NUM_END, counter);
+            threads[i] = new pc_dynamic_thread(i, NUM_END, SIZE_OF_TASK, task_stack, counter);
             threads[i].start();
         }
         for (i=0; i<NUM_THREADS; i++) {
@@ -35,36 +39,33 @@ public class pc_static_cyclic {
     }
 }
 
-class pc_static_cyclic_thread extends Thread {
-    int thread, num_of_threads, size_of_task, num_start, num_end;
-    pc_static_cyclic_counter counter;
-    public pc_static_cyclic_thread(
+class pc_dynamic_thread extends Thread {
+    final int thread, num_end, size_of_task;
+    pc_dynamic_counter counter;
+    pc_dynamic_task_stack task_stack;
+    public pc_dynamic_thread(
             int thread,
-            int num_of_threads,
-            int size_of_task,
-            int num_start,
             int num_end,
-            pc_static_cyclic_counter counter
+            int size_of_task,
+            pc_dynamic_task_stack task_stack,
+            pc_dynamic_counter counter
     ) {
         this.thread = thread;
-        this.num_of_threads = num_of_threads;
-        this.size_of_task = size_of_task;
-        this.num_start = num_start;
         this.num_end = num_end;
+        this.size_of_task = size_of_task;
+        this.task_stack = task_stack;
         this.counter = counter;
     }
 
     @Override
     public void run() {
         long startTime = System.currentTimeMillis();
-        for (int i=0; (i * size_of_task * num_of_threads) < num_end; i++) {
-            for (int j=0; j < size_of_task; j++) {
-                int number_to_check = (i * num_of_threads * size_of_task) + (size_of_task * thread) + j;
-                if (number_to_check < num_end) {
-                    if (isPrime(number_to_check)) counter.addCount();
-                } else {
-                    break;
-                }
+        while (true) {
+            OptionalInt get_task = task_stack.getTask();
+            if (get_task.isEmpty()) break;
+            int task = get_task.getAsInt();
+            for (int i = task; i < Math.min(task + size_of_task, num_end); i++) {
+                if (isPrime(i)) counter.addCount();
             }
         }
         long endTime = System.currentTimeMillis();
@@ -82,9 +83,27 @@ class pc_static_cyclic_thread extends Thread {
     }
 }
 
-class pc_static_cyclic_counter {
+class pc_dynamic_counter {
     int num_of_prime_numbers = 0;
     synchronized void addCount() {
         this.num_of_prime_numbers += 1;
+    }
+}
+
+class pc_dynamic_task_stack {
+    final int num_start, num_end, size_of_task;
+    int current_task;
+
+    public pc_dynamic_task_stack(int num_start, int num_end, int size_of_task) {
+        this.num_start = num_start;
+        this.num_end = num_end;
+        this.size_of_task = size_of_task;
+        this.current_task = num_start;
+    }
+
+    synchronized OptionalInt getTask() {
+        if (current_task >= num_end) return OptionalInt.empty();
+        current_task += size_of_task;
+        return OptionalInt.of(current_task - size_of_task);
     }
 }
